36cbf14d45621bed7e1e145b6c584b688f99f375
keertirajmalik <keertirajmalik@gmail.com>
2020-08-04 21:40:10 +0530

    simple chat code usin socket.io

--- app.js
+++ app.js
@@ -1,42 +1,33 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
-const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//Code send message to server
+  
+// io.on('connection', (socket) => {
+//     console.log('user connected');
+//     socket.on('chat message', (msg) => {
+//         console.log('message: ' + msg);
+//     });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+//     // Code which disconnets the user
+//     // socket.on('disconnect', () => {
+//     //     console.log('user disconnected');
+//     // });
+// });
+
+// Code to broadcast message
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
 });
-
-module.exports = app;
--- sockets.js
+++
@@ -1,9 +0,0 @@
-const io = require('socket.io-client')();
-
-let socket = io.connect('http://localhost:3000/about');
-
-socket.on('welcome', (msg) => {
-    console.log(msg);
-});
-
-socket.emit('about')
